4d0daf62eb065277973e2fd468ef9a816b926419
Jake Soenneker <jake@soenneker.com>
2023-03-19 11:21:38 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Extensions.FileInfo.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.FileInfo/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.extensions.fileinfo/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.extensions.fileinfo/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Extensions.FileInfo.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.FileInfo/)
+
+# ![]({Package:Icon75x75}) Soenneker.Extensions.FileInfo
+### A collection of helpful FileInfo extension methods
+
+## Installation
+
+```
+Install-Package Soenneker.Extensions.FileInfo
+```
---
+++ src/FileInfoExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace Soenneker.Extensions.FileInfo;
+
+public static class FileInfoExtension
+{
+    public static bool HasReadOnlyOrArchivedAttribute(this System.IO.FileInfo value)
+    {
+        if (HasAttributeSet(value, FileAttributes.Archive))
+            return true;
+
+        if (HasAttributeSet(value, FileAttributes.ReadOnly))
+            return true;
+
+        return false;
+    }
+
+    public static void RemoveReadOnlyOrArchivedAttribute(this System.IO.FileInfo value)
+    {
+        RemoveAttribute(value, FileAttributes.Archive);
+        RemoveAttribute(value, FileAttributes.ReadOnly);
+    }
+
+    public static bool HasAttributeSet(this System.IO.FileInfo value, FileAttributes pAttributes)
+    {
+        return pAttributes == (value.Attributes & pAttributes);
+    }
+
+    public static void RemoveAttribute(this System.IO.FileInfo value, FileAttributes pAttributes)
+    {
+        value.Attributes = value.Attributes & ~pAttributes;
+        value.Refresh();
+    }
+}
