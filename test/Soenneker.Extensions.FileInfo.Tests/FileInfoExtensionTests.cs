853034dd674bffb0e2828eadbfa579dfe9db4910
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:20 +0000

    Tests

---
+++ test/Soenneker.Extensions.FileInfo.Tests/FileInfoExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.FileInfo.Tests;
+
+public class FileInfoExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
